c5c52e462fc31e79c9d32fc2f5ce1acaaa4c2684
Kevin <Jonathanss999@gmail.com>
2025-05-06 15:10:37 +0700

    Update DatabaseSeeder.php

--- latihan/database/seeders/DatabaseSeeder.php
+++ latihan/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@example.com',
+            'password' => Hash::make('password'),
         ]);
+
+        // insert data mahasiswa menggunakan query builder
+        DB::table('mahasiswa')->insert([
+            'npm' => '2428250074',
+            'nama_mahasiswa' => 'Krisna Pramudya',
+            'tempat_lahir' => 'Palembang',
+            'tanggal_lahir' => '2001-07-15',
+            'alamat' => 'Palembang'
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('mahasiswa')
+        -> where("npm", "2428250074")
+        -> update(["npm", "2428250074P"])
     }
 }
